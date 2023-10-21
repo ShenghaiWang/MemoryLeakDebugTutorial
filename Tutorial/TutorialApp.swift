49db3288eec92ef25d8597a94700256e5edcef70
Tim Wang <overocean@gmail.com>
2023-10-21 15:28:52 +1100

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Tim Wang on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
